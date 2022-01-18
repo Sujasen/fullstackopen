464532f5bb887bac50653c2221fb0876dd8e85eb
Sujasen <sujason39@gmail.com>
2022-01-17 17:14:59 -0800

    MongoDB implemented

--- part_3/index.js
+++ part_3/index.js
@@ -22,10 +22,15 @@ let notes = [
 ////////////////////////////
 
 // Importing express and storing application into 'app'
-const express = require('express')   
-const app     = express()
+require('dotenv').config()
+const express  = require('express')
+const mongoose = require('mongoose')
+const cors     = require('cors')
+const Note     = require('./models/note')
 
-const cors = require('cors')
+
+
+const app      = express()
 
 /// Middleware
 const requestLogger = (request, response, next) => {
@@ -36,11 +41,24 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
+const errorHandler = (error, request, response, next) => {
+    console.error("middleware: ", error.message)
+
+    if(error.name === 'CastError'){
+        return response.status(400).send({ error:"Middleware: Malformatted id" })
+    } else if(error.name === 'ValidationError'){
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
 // Allows request.body from POST to become an object
-app.use(express.json())
-app.use(requestLogger)
 app.use(cors())
 app.use(express.static('build')) // Make express show static content (index.html and JS)
+app.use(express.json())
+app.use(requestLogger)
+
 
 
 
@@ -51,29 +69,51 @@ app.get('/', (request, response) => {
 
 //Route2: HTTP GET notes
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+   Note.find({}).then(notes => {
+       response.json(notes)
+   })
 })
 
 //Route3: GET single notes
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if(note){
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+
+    // const id = Number(request.params.id)
+    // const note = notes.find(note => note.id === id)
     
-    if(note)
-    {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    // if(note)
+    // {
+    //     response.json(note)
+    // } else {
+    //     response.status(404).end()
+    // }
 
 })
 
 //Route4: DELETE note
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+         .then(result => {
+             if(result){
+                response.send(`Note ID ${request.params.id} deleted.`).end()
+             } else {
+                 response.status(404).send(`Note ID ${request.params.id} not found. No Action taken.`).end()
+             }
+         })
+         .catch(error => next(error))
+    
+    // const id = Number(request.params.id)
+    // notes = notes.filter(note => note.id !== id)
 
-    response.status(204).end()
+    // response.status(204).end()
 })
 
 const generateId = () => {
@@ -83,34 +123,52 @@ const generateId = () => {
 }
 
 //Route5: POST note
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-    if(!body.content){
+    if(body.content === undefined){
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    const note = {
-        id: generateId(),
+    const note = new Note({
+        //id: generateId(),
         content: body.content,
         important: body.important || false,
         date: new Date()
-    }
+    })
     console.log("newNote", note);
 
-    notes = notes.concat(note)
-    
-    response.json(note)
+    note.save()
+        .then(saveNote => {
+            response.json(saveNote)
+        })
+        .catch(error => next(error))
+
+    // notes = notes.concat(note)
+    // response.json(note)
 })
 
 //Route6: PUT note
-app.put('/api/notes/:id', (request, response) => {
-    notes = notes.map(temp => temp.id !== request.body.id ? temp : request.body)
-    response.json(request.body)
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+         .then(updatedNote => {
+             response.json(updatedNote)
+         })
+         .catch(error => next(error))
+    // notes = notes.map(temp => temp.id !== request.body.id ? temp : request.body)
+    // response.json(request.body)
 })
 
-const PORT =  process.env.PORT || 3002
+const PORT =  process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
+
+app.use(errorHandler)
