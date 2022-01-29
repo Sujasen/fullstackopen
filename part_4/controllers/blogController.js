32fb60700bcbd42ead5597d0fa63bb7237af3a70
Sujasen <sujason39@gmail.com>
2022-01-29 14:06:01 -0800

    Added Update/Delete Routers to BlogController

--- part_4/controllers/blogController.js
+++ part_4/controllers/blogController.js
@@ -3,14 +3,12 @@ const blogRouter = require('express').Router()
 const blogObj    = require('../models/blogSchema')
 const logger     = require('../utils/logger')
 
-blogRouter.get('/', (request, response) => {
-    blogObj.find({})
-           .then(result => {
-               response.json(result)
-           })
+blogRouter.get('/', async (request, response) => {
+    const result = await blogObj.find({})
+    return response.json(result)
 })
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
     const body = request.body
 
     if(!body.likes){
@@ -32,11 +30,36 @@ blogRouter.post('/', (request, response) => {
         likes:  body.likes
     })
 
-    newBlog.save()
-           .then(result => {
-                response.json(result)
-           })
-           .catch(error => logger.error(error))
+    const result = await newBlog.save()
+
+     return response.json(result)
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+    const result = await blogObj.findByIdAndDelete(request.params.id)
+
+    if(result){
+        response.status(204).send(`${request.params.id} deleted`)
+    } else {
+        response.send(`${request.params.id} not found. No action taken`)
+    }
+})
+
+blogRouter.put('/:id', async (request, response) => {
+    const body = request.body
+    const newBlog = {
+        author: body.author,
+        likes: body.likes,
+        title: body.title,
+        url: body.url
+    }
+    const result = await blogObj.findOneAndUpdate(request.params.id, newBlog, {new: true} )
+
+    if(result){
+        response.status(204).json(result)
+    } else {
+        response.send(`${request.params.id} not found. No action taken`)
+    }
 })
 
 
--- part_4/tests/blog.test.js
+++ part_4/tests/blog.test.js
@@ -5,6 +5,7 @@ const app        = require('../app')
 const api        = supertest(app)
 
 const Blog      = require('../models/blogSchema')
+const { update } = require('lodash')
 
 beforeEach( async () => {
     // Wait on deleting Database entries
@@ -84,14 +85,46 @@ describe('BLOG TESTING SUITE', () => {
                  .send(newBlog)
                  .expect(400)
     })
-    
 
-    afterAll(() => {
-        mongoose.connection.close()
+    ///////////////////////////////////
+    test('Deleting single blog', async () => {
+        const blogStart = await blogHelper.getBlogs()
+        const deleteBlog = blogStart[0]
+
+        await api.delete(`/api/blogs/${deleteBlog.id}`)
+            .expect(204)
+
+        const blogEnd = await blogHelper.getBlogs()
+
+        expect(blogEnd).toHaveLength(blogHelper.initialBlogs.length - 1)
+        
+        const titles = blogEnd.map(r => r.title)
+        expect(titles).not.toContain(deleteBlog.title)
+        
     })
 
+    test('Updating single blog', async () => {
+        const blogStart = await blogHelper.getBlogs()
+        let updateBlog = blogStart[0]
+
+        updateBlog.likes = 88888888888
+
+        await api.put(`/api/blogs/${updateBlog.id}`)
+                 .send(updateBlog)
+                 .expect(204)
+
+        const blogEnd = await blogHelper.getBlogs()
+        console.log(blogEnd)
+        const updateLike = blogEnd.find(r => r.likes === updateBlog.likes)
+
+        expect(updateLike.likes).toBe(updateBlog.likes)
+    })
+})
 
 
+    
+    
 
-
+afterAll(() => {
+    mongoose.connection.close()
 })
